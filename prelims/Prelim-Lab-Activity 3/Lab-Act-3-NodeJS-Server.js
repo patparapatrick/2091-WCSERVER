f0f50f0a6a34fd7bc82d626b36c4350501499113
patparapatrick <123344385+patparapatrick@users.noreply.github.com>
2023-01-30 12:39:38 +0800

    1 file modified

---
+++ prelims/Prelim-Lab-Activity 3/Lab-Act-3-NodeJS-Server.js
@@ -0,0 +1,28 @@
+// Task Name: Lab 3
+// Villanueva, Patrick C.
+// WD201
+
+var http = require('http');
+var server = http.createServer(function(req, res){
+  if (req.url == '/'){
+    res.WriteHead(200, {'Content-Type': 'text/html'});
+
+    res.write('<html><body><p>This is home Page.</p></body></html>');
+    res.end();
+  }
+  else if (req.url == "/student"){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<html><body><p>This is student Page.</p></body></html>');
+    res.end();
+  }
+  else if (req.url == "/admin"){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<html><body><p>This is admin Page.</p></body></html>');
+    res.end();
+  else{
+    res.end('Invalid Request!');
+
+}
+
+server.listen(5000);
+console.log('Node.js webserver at port 5000 is running..')
