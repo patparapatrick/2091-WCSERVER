3de22343c1bdd8904e79a03c50f91dcd2b578fb0
patparapatrick <123344385+patparapatrick@users.noreply.github.com>
2023-01-30 16:40:32 +0800

    1 file modified, 1 added

--- prelims/Prelim-Lab-Activity 3/Lab-Act-3-NodeJS-Server.js
+++ prelims/Prelim-Lab-Activity 3/Lab-Act-3-NodeJS-Server.js
@@ -8,17 +8,21 @@ var server = http.createServer(function (req, res){
   if (req.url == '/'){
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    res.write('<html><body><p>This is home Page.</p></body></html>');
+    res.write('<html><body style = "background-color: skyblue"><h1 style="color: white">Welcome to my Node.js Application</h1><br><p>Welcome " +name+ "This is an activity about basics of Node.js</p></body></html>');
     res.end();
   }
-  else if (req.url == "/student"){
+  else if (req.url == "/about"){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<html><body><p>This is student Page.</p></body></html>');
+    res.write('<html><body style = "background-color: #ff6961"><h1 style = "color: white">This is the About Page.</h1><p>Hello Patrick. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>');
     res.end();
   }
-  else if (req.url == "/admin"){
+  else if (req.url == "/contact"){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<html><body><p>This is admin Page.</p></body></html>');
+    res.write('<html><body style = "background-color: #FDFD96"><h1 style="color: red">This is the Contact Page</h1><p>Patrick Villanueva, if you want additional details about this activity, go to his site: https://www.tutorialsteacher.com</p></body></html>');
+    res.end();
+  }else if (req.url == "/gallery"){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<html><body style = "background-color: #77DD77"><h1 style="color: white">This is Gallery Page.</h1></body></html>');
     res.end();
   }
   else
---
+++ prelims/Prelim-Lab-Activity 3/call-Lab-3.js
@@ -0,0 +1,4 @@
+var call = require('./Lab-Act-3-NodeJS-Server.js');
+
+console.log(greetings("Villanueva Patrick"));
+
