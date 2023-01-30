88181f1b91bf5e12d4ea5cb76a6941a699473f9b
patparapatrick <123344385+patparapatrick@users.noreply.github.com>
2023-01-30 21:56:34 +0800

    2 files modified

--- prelims/Prelim Hands On Quiz/app.js
+++ prelims/Prelim Hands On Quiz/app.js
@@ -1,7 +1,7 @@
 
-var sum = require('./calculation.js');
+var calculate = require('./calculation.js');
 
-console.log('10 + 2 = ',sum(10, 2));
-console.log('10 - 2 = ',subtract(10, 2));
-console.log('10 * 2 = ',multiply(10, 2));
-console.log('10 / 2 = ',divide(10, 2));
+console.log('10 + 2 = ',calculate.sum(10, 2));
+console.log('10 - 2 = ',calculate.subtract(10, 2));
+console.log('10 * 2 = ',calculate.multiply(10, 2));
+console.log('10 / 2 = ',calculate.divide(10, 2));
--- prelims/Prelim Hands On Quiz/calculation.js
+++ prelims/Prelim Hands On Quiz/calculation.js
@@ -14,8 +14,11 @@ function divide(a,b) {
   return a / b;
 }
 
-module.exports = add;
-module.exports = subtract;
-module.exports = multiply;
-module.exports = divide;  
+module.exports = {
+  add: (a,b) => a+b,
+  subtract: (a,b) => a-b,
+  multiply: (a,b) => a*b,
+  divide: (a,b) => a/b,
+};
+
 
