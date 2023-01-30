db2692cf92e9b055cc0329ecb1eb62d1314c91de
patparapatrick <123344385+patparapatrick@users.noreply.github.com>
2023-01-30 12:42:45 +0800

    1 file modified

--- prelims/Prelim-Lab-Activity 3/Lab-Act-3-NodeJS-Server.js
+++ prelims/Prelim-Lab-Activity 3/Lab-Act-3-NodeJS-Server.js
@@ -19,10 +19,12 @@ var server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<html><body><p>This is admin Page.</p></body></html>');
     res.end();
-  else{
+  }
+  else
     res.end('Invalid Request!');
 
-}
+  });
+  
 
 server.listen(5000);
 console.log('Node.js webserver at port 5000 is running..')
