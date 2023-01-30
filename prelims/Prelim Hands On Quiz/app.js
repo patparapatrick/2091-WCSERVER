0264271a2ff695b5004136136817b17d3b0b33af
patparapatrick <123344385+patparapatrick@users.noreply.github.com>
2023-01-30 21:35:55 +0800

    1 file modified

--- prelims/Prelim Hands On Quiz/app.js
+++ prelims/Prelim Hands On Quiz/app.js
@@ -1,6 +1,6 @@
 var calculate = require('./calculation.js');
 
-console.log('Addition: ', calculate.add(10, 2));
+console.log('Addition: ', add(10, 2));
 console.log('Subtraction: ', calculate.subtract(10,2));
 console.log('Multiplication: ', calculate.multiply(10,2));
 console.log('Division: ', calculate.divide(10,2));
