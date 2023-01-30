f47a1664951af211cbcdfe36d995f4b2c5dfe596
patparapatrick <123344385+patparapatrick@users.noreply.github.com>
2023-01-30 21:57:46 +0800

    1 file modified

--- prelims/Prelim Hands On Quiz/calculation.js
+++ prelims/Prelim Hands On Quiz/calculation.js
@@ -1,18 +1,3 @@
-function add(a,b) {
-  return a + b;
-}
-
-function subtract(a,b) {
-  return a - b;
-}
-
-function multiply(a,b) {
-  return a * b;
-}
-
-function divide(a,b) {
-  return a / b;
-}
 
 module.exports = {
   add: (a,b) => a+b,
